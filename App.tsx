ea6e8c4dd64dcae2250c2a280edd14f61e6a75d5
alisson <alissonbrg32@gmail.com>
2025-02-25 17:49:17 -0300

    Making onboarding

--- App.tsx
+++ App.tsx
@@ -1,13 +1,54 @@
-import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
+import React, { useState } from 'react';
+import { Pressable, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+
+  const pages = [<Welcome/>, <Text/>, <Text/>]
+
+  const [stepIndex, setStepIndex] = useState(0)
+
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx" />
+    <SafeAreaProvider>
+    <SafeAreaView className='flex-1 justify-center items-center p-none bg-night'>
+      {pages[stepIndex]}
+      <View className='h-48 justify-end p-10'>
+        <View className='mx-auto my-10 w-36 justify-between flex-row'>
+          {
+            pages.map((_,i)=><Circle selected={stepIndex} index={i} key={i}/>)
+          }    
+        </View>
+        {stepIndex < 2 &&
+        <Pressable
+        onPress={()=>setStepIndex(stepIndex+1)}
+        className='bg-crimson p-5 w-96 rounded-md'>
+          <Text className='text-silver text-center font-bold'>Começar agora!</Text>
+        </Pressable>
+        }
+      </View>
       <StatusBar style="auto" />
-    </>
+    </SafeAreaView>
+    </SafeAreaProvider>
   );
+}
+
+function Circle(props: {selected: number, index: number}){
+  return <View
+  className={`w-5 h-5 border border-emerald rounded-full ${props.selected == props.index ? "bg-emerald" : ""}`}></View>
+}
+
+function Welcome(){
+  return <View className="flex-1 justify-center bg-night">
+          <Text className="text-[2.2rem] text-silver w-96 text-left">Bem vindo ao <Text className='text-emerald font-bold'>Anout</Text></Text>
+          <Text className='mt-10 text-xl text-silver w-96 text-left'>Seu novo app favorito para organizar seus afazeres.{("\n\n")}Aqui, irá criar listas, to-dos e configura-los para maximizar sua produtividade e não esquecer do que você tem que fazer.</Text>
+        </View>;
+}
+
+function Account(){
+  return <View>
+    
+  </View>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        crimson: "#DE1544",
+        emerald: "#c1dc45",
+        silver: "#E0E0E0", // Ajustado para um branco acinzentado mais claro
+        night: "#1A202C", // Ajustado para uma cor escura menos intensa
+        dark: "#121826", // Ajustado para uma cor mais escura que night
+      },
+    },
   },
   plugins: [],
 };
